1ed6e5737e2b9dfeb500dce43e095e2e0b08d541
Brian Young <by12380@gmail.com>
2017-09-18 17:34:51 -0700

    PART-2 Fixing a bug

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3b1AaGB1hEJOcdiZt86oK6LKkdf2jn1A",
+    authDomain: "oshop-4bf90.firebaseapp.com",
+    databaseURL: "https://oshop-4bf90.firebaseio.com",
+    projectId: "oshop-4bf90",
+    storageBucket: "",
+    messagingSenderId: "332657368014"
+  }
+};
