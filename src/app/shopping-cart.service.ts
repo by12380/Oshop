3afdc0d8fcb919241b017714dfc9d1cf2f9ab71c
Brian Young <by12380@gmail.com>
2017-09-22 00:46:36 -0700

    PART-5 Adding a product to the shopping cart

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,7 @@
+import { Product } from './models/product';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/take'
 
 @Injectable()
 export class ShoppingCartService {
@@ -7,7 +9,7 @@ export class ShoppingCartService {
   constructor(private db: AngularFireDatabase) { }
 
   private create(){
-    return this.db.list('/shopping-cart').push({
+    return this.db.list('/shopping-carts').push({
       dateCreate: new Date().getTime()
     });
   }
@@ -16,15 +18,22 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId() {
     let cartId = localStorage.getItem('cartId');
-    if (!cartId){
-      let result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return this.getCart(result.key);
-    }
-        
-    return this.getCart(cartId);
+    if (cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.$key);
+    item$.take(1).subscribe(item => {
+      if (item.$exists()) item$.update({ quantity: item.quantity + 1});
+      else item$.set({ product: product, quantity: 1 });
+    })
   }
 
 }
