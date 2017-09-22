3e21eed39a0817286973e97e01c1dded7859bf34
Brian Young <by12380@gmail.com>
2017-09-22 00:15:01 -0700

    PART-5 Refactoring: Moving the responsibility to the service

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -14,15 +14,7 @@ export class ProductCardComponent {
   constructor(private cartService: ShoppingCartService) { }
 
   addToCart(product: Product) {
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId){
-      this.cartService.create().then(result => {
-        localStorage.setItem('cartId', result.key);
-
-        //Add product to cart
-      });
-    }
-    //Add product to cart
+    this.cartService.addToCart(product);
   }
 
 }
